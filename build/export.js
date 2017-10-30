1e5e6d454f851b675202b0c113d0b5329d851c25
Jan Forst <ensonador@gmail.com>
2017-10-31 00:19:11 +0100

    verbosed, simplified export

--- build/export.js
+++ build/export.js
@@ -1,41 +1,29 @@
 const fs = require('fs')
 const meta = require('../meta/meta.json')
 const mkdirp = require('mkdirp')
-// let compact = require('json-stringify-pretty-compact')
 
 mkdirp.sync('icons')
 
-let index = meta.reduce((r, m) => {
-  r[m.name.replace(/ /g, '_')] = m.data || meta[m.link].data
-  return r
-}, {})
-
-index = Object.keys(index).sort().reduce((r, name) => {
-  r[name] = index[name]
-  return r
-}, {})
-
-// fs.writeFileSync('index.json', compact(index, {maxLength: 1024}))
+console.log('Underscoring names')
+meta.forEach(m => m.name = m.name.replace(/ /g, '_'))
 
 let ind = ['/* eslint-disable camelcase */']
 
-meta.map(d => d.name).sort().forEach(k => {
-  let m = meta.find(d => d.name === k)
-  let n = m.name.replace(/ /g, '_')
-
-  let f
+meta.forEach(m => {
+  let n = m.name
+  let f = `icons/${n}.js`
 
   if (m.link >= 0) {
-    const o = meta[m.link].name.replace(/ /g, '_')
-    let t = `export {default} from './${o}'`
-    f = `icons/${n}.js`
-    fs.writeFile(f, t, () => {})
+    const o = meta[m.link].name
+    console.log(`Linking: ${n} --> ${o}`)
+    fs.writeFile(f, `export {default} from './${o}'`, () => {})
   } else {
-    f = `icons/${n}.js`
+    console.log(`Writing: ${n}`)
     fs.writeFile(f, `export default ${JSON.stringify(m.data)}`, () => {})
   }
 
   ind.push(`export {default as ${n}} from './icons/${n}'`)
 })
 
+console.log(`Writing: index.js`)
 fs.writeFile('index.js', ind.join('\n'), () => {})
