4fba171a57e9be126c520aa4661277e45b5af14a
Jan Forst <ensonador@gmail.com>
2017-11-06 11:37:14 +0100

    improving compare performance

--- build/compare.js
+++ build/compare.js
@@ -3,21 +3,39 @@ const meta = require('../meta/_rendered.json')
 const conf = require('./config/icon.conf')
 const maxLimit = conf.distance
 
+console.time('Comparing');
+
+// const cache = {}
 let similar = []
 let l = meta.length
+
+const m1 = 0x55555555
+const m2 = 0x33333333
+const m4 = 0x0f0f0f0f
+const m8 = 0x00ff00ff
+const m16 = 0x0000ffff
+
+function count (x) {
+  x = (x & m1) + ((x >> 1) & m1)
+  x = (x & m2) + ((x >> 2) & m2)
+  x = (x & m4) + ((x >> 4) & m4)
+  x = (x & m8) + ((x >> 8) & m8)
+  x = (x & m16) + ((x >> 16) & m16)
+  return x
+}
 
 for (let x = 0; x < l; x++) {
   for (let y = x + 1; y < l; y++) {
     const limit = Math.min(maxLimit, Math.min(meta[x].pixels, meta[y].pixels))
     const diff = Math.abs(meta[x].pixels - meta[y].pixels)
 
-    if(diff > limit) {
+    if (diff > limit) {
       continue
     }
 
     const d = getDelta(meta[x].hash, meta[y].hash, limit)
 
-    if(d < limit) {
+    if (d < limit) {
       similar.push([x, y, d])
     }
   }
@@ -26,6 +44,8 @@ for (let x = 0; x < l; x++) {
 similar.sort((a, b) => {
   return a[2] - b[2]
 })
+
+console.timeEnd('Comparing');
 
 fs.writeFileSync('meta/_similar.json', JSON.stringify(similar))
 
@@ -36,10 +56,34 @@ function getDelta (a, b, limit) {
 
   let d = 0
 
+  for (let i = 0; i < a.length; i += 8) {
+    // const as = a.substr(i, 8)
+    // const an = cache[as] || (cache[as] = parseInt(as, 16))
+    // const bs = b.substr(i, 8)
+    // const bn = cache[bs] || (cache[bs] = parseInt(bs, 16))
+
+    // d += count(an ^ bn)
+    d += count(parseInt(a.substr(i, 8), 16) ^ parseInt(b.substr(i, 8), 16))
+
+    if (d >= limit) {
+      return d
+    }
+  }
+
+  return d
+}
+
+function getDelta2 (a, b, limit) {
+  if (a === b) {
+    return 0
+  }
+
+  let d = 0
+
   for (let i = 0; i < a.length; i++) {
     d += getSubDelta(a[i], b[i])
 
-    if(d >= limit) {
+    if (d >= limit) {
       return d
     }
   }
--- build/render.js
+++ build/render.js
@@ -3,11 +3,15 @@ const fs = require('fs')
 const renderSvg = require('./lib/renderSvg')
 const data = require('../meta/_loaded.json')
 
+console.time('Rendering')
+
 Promise.all(data.map(d => renderSvg(d.data))).then(files => {
   files.forEach((f, i) => {
     data[i].hash = f.hash
     data[i].pixels = f.pixels
   })
+
+  console.timeEnd('Rendering')
 
   fs.writeFileSync('meta/_rendered.json', compactJSON(data, {maxLength: 2048}))
 })
